4377b73c3a8abed9a6af89ab9eb997dc628afde3
Scriptopathe <josue.alvarez@hotmail.fr>
2017-01-22 09:42:00 +0100

    update scripts

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 # Run as sudo
 
-apt-get install node
-apt-get install mongodb
-apt-get install nodejs
-apt-get install python
+echo "Installing node"
+apt-get install node -y
+echo "Installing mongodb"
+apt-get install mongodb -y
+echo "Installing nodejs"
+apt-get install nodejs -y
+echo "Installing python"
+apt-get install python -y
 
+
+echo "Installing node packages"
 npm install -g typescript
 npm install -g tsd
 npm install -g node-dev 
 npm install -g ts-node
 
+echo "Installing mosquitto"
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 apt-key add mosquitto-repo.gpg.key
 cd /etc/apt/sources.list.d/
 wget http://repo.mosquitto.org/debian/mosquitto-wheezy.list
 apt-get update
-apt-get install mosquitto
-apt-get install mosquitto mosquitto-clients python-mosquitto
+apt-get install mosquitto -y
+apt-get install mosquitto mosquitto-clients python-mosquitto -y
